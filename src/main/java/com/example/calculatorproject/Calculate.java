61b266dea7b896f808ad920f4a1dfdd00f201886
Milad Tavakoli <milad.tavakoli81@gmail.com>
2022-12-23 23:47:55 +0330

    writing calculate methods

--- src/main/java/com/example/calculatorproject/Calculate.java
+++ src/main/java/com/example/calculatorproject/Calculate.java
@@ -1,7 +1,122 @@
 package com.example.calculatorproject;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Map;
 import java.util.Stack;
 
 public class Calculate {
-    Stack<String> equation = new Stack<>();
+    public static String equation = "";
+
+    public static double calculate() {
+        String postFix = infixToPostfix(equation);
+        Stack<String> stack = new Stack<>();
+        double x, y;
+        String result = "";
+        String choice;
+        double value;
+        String p = "";
+        String[] split = postFix.split(" ");
+        for (int i = 0; i < split.length; i++) {
+            String temp = split[i];
+            if (!temp.equals("+") && !temp.equals("-")
+                    && !temp.equals("*") && !temp.equals("/") && !temp.equals("^")) {
+                stack.push(temp);
+                continue;
+            } else {
+                choice = temp;
+            }
+            switch (choice) {
+                case "+":
+                    x = Double.parseDouble(stack.pop());
+                    y = Double.parseDouble(stack.pop());
+                    value = x + y;
+                    result = p + value;
+                    stack.push(result);
+                    break;
+                case "-":
+                    x = Double.parseDouble(stack.pop());
+                    y = Double.parseDouble(stack.pop());
+                    value = y - x;
+                    result = p + value;
+                    stack.push(result);
+                    break;
+                case "*":
+                    x = Double.parseDouble(stack.pop());
+                    y = Double.parseDouble(stack.pop());
+                    value = x * y;
+                    result = p + value;
+                    stack.push(result);
+                    break;
+
+                case "/":
+                    x = Double.parseDouble(stack.pop());
+                    y = Double.parseDouble(stack.pop());
+                    value = y / x;
+                    result = p + value;
+                    stack.push(result);
+                    break;
+
+                case "^":
+                    x = Double.parseDouble(stack.pop());
+                    y = Double.parseDouble(stack.pop());
+                    value = Math.pow(y, x);
+                    result = p + value;
+                    stack.push(result);
+                    break;
+
+                default:
+                    continue;
+            }
+        }
+        return Double.parseDouble(result);
+    }
+
+    static int Prec(String ch) {
+        return switch (ch) {
+            case "+", "-" -> 1;
+            case "*", "/" -> 2;
+            case "^" -> 3;
+            default -> -1;
+        };
+    }
+
+    static String infixToPostfix(String equation) {
+        String result = "";
+        Stack<String> stack = new Stack<>();
+        String split[] = equation.split(" ");
+        for (String c : split) {
+            if (!(c.equals("+") || c.equals("-") || c.equals("*") || c.equals("/") || c.equals("^") || c.equals("(") || c.equals(")"))) {
+                result += " ";
+                result += c;
+            } else if (c.equals("("))
+                stack.push(c);
+            else if (c.equals(")")) {
+                while (!stack.isEmpty()
+                        && !stack.peek().equals("(")) {
+                    result += " ";
+                    result += stack.peek();
+                    stack.pop();
+                }
+
+                stack.pop();
+            } else {
+                while (!stack.isEmpty()
+                        && Prec(c) <= Prec(stack.peek())) {
+                    result += " ";
+                    result += stack.peek();
+                    stack.pop();
+                }
+                stack.push(c);
+            }
+        }
+        while (!stack.isEmpty()) {
+            if (stack.peek().equals("("))
+                return "Invalid Expression";
+            result += " ";
+            result += stack.peek();
+            stack.pop();
+        }
+        return result;
+    }
 }
