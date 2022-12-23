3a6ae6e71ca50921631f88611e552a3ca2d31c61
Milad Tavakoli <milad.tavakoli81@gmail.com>
2022-12-23 17:21:14 +0330

    design calculator ui

--- src/main/java/com/example/calculatorproject/HelloController.java
+++ src/main/java/com/example/calculatorproject/HelloController.java
@@ -1,14 +1,102 @@
 package com.example.calculatorproject;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.text.Text;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Text mainText;
+
+    @FXML
+    void closedParenthesisButtonClick(ActionEvent event) {
+
     }
+
+    @FXML
+    void divisionButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void eightButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void equalButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fiveButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fourButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void minusButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void multiplictionButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void nineButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void oneButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openParenthesisButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void plusButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void powerButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sevenButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sixButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void threeButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void twoButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void zeroButtonClick(ActionEvent event) {
+
+    }
+
 }
